823bd2e230fc98614ec1e3d0d25f81e63f37f2af
Sourav Saha <souravsaha@Souravs-MacBook-Air.local>
2025-02-06 15:34:21 +0530

    StudentExport update

--- app/Exports/StudentsExport.php
+++ app/Exports/StudentsExport.php
@@ -14,42 +14,48 @@ class StudentsExport implements FromCollection, WithHeadings
         // echo "<pre>";
         // print_r(StudentDetail::with(['address', 'qualifications'])->get()->toArray());
         // die;
-        return (StudentDetail::with(['address', 'qualifications'])->get())->map(function ($student) {
-            // dd($student->qualifications);
-            return [
-                'Full Name' => $student->firstname . ' ' . $student->middlename . ' ' . $student->lastname,
-                'Email' => $student->email,
-                'Mobile No' => $student->mobile_no,
-                'Current Address' => $student->address ? $student->address->current_house_no . ', ' . $student->address->current_street . ', ' . $student->address->current_district . ', ' . $student->address->current_state . ' - ' . $student->address->current_pincode : 'Not Provided',
-                'Permanent Address' => $student->address ? $student->address->permanent_house_no . ', ' . $student->address->permanent_street . ', ' . $student->address->permanent_district . ', ' . $student->address->permanent_state . ' - ' . $student->address->permanent_pincode : 'Not Provided',
-                'Aadhaar No' => $student->aadhaar_no,
-                'Gender' => $student->gender,
-                'DOB' => $student->dob,
-                'Age' => $student->age,
-                'Nationality' => $student->nationality,
-                'Country' => $student->address->country ?? 'N/A',
+
+        return collect(StudentDetail::with(['address', 'qualifications'])->get()->toArray())->map(function ($student) {
+
+            $qualifications = collect($student['qualifications'] ?? []);
+
+            $studentData = [
+                'Full Name' => $student['firstname'] . ' ' . $student['middlename'] . ' ' . $student['lastname'],
+                'Email' => $student['email'],
+                'Mobile No' => $student['mobile_no'],
+                'Current Address' => isset($student['address']) 
+                    ? $student['address']['current_house_no'] . ', ' . $student['address']['current_street'] . ', ' . $student['address']['current_district'] . ', ' . $student['address']['current_state'] . ' - ' . $student['address']['current_pincode']
+                    : 'Not Provided',
+                'Permanent Address' => isset($student['address']) 
+                    ? $student['address']['permanent_house_no'] . ', ' . $student['address']['permanent_street'] . ', ' . $student['address']['permanent_district'] . ', ' . $student['address']['permanent_state'] . ' - ' . $student['address']['permanent_pincode']
+                    : 'Not Provided',
+                'Aadhaar No' => $student['aadhaar_no'],
+                'Gender' => $student['gender'],
+                'DOB' => $student['dob'],
+                'Age' => $student['age'],
+                'Nationality' => $student['nationality'],
+                'Country' => $student['address']['country'] ?? 'N/A',
             ];
 
             $highSchool = '';
             $intermediate = '';
             $graduation = '';
             $postGraduation = '';
-            
-            if ($student->qualifications->isNotEmpty()) {
-                foreach ($student->qualifications as $qualification) {
-                    // dd($qualification);
-                    switch ($qualification->qualification_type) {
+
+            if ($qualifications->isNotEmpty()) {
+                foreach ($qualifications as $qualification) {
+                    switch ($qualification['qualification_type']) {
                         case 'High School':
-                            $highSchool = $qualification->institution_name . ' (' . $qualification->passing_year . ', ' . $qualification->percentage . '%)';
+                            $highSchool = $qualification['institution_name'] . ' (' . $qualification['passing_year'] . ', ' . $qualification['percentage'] . '%)';
                             break;
                         case 'Intermediate':
-                            $intermediate = $qualification->institution_name . ' (' . $qualification->passing_year . ', ' . $qualification->percentage . '%)';
+                            $intermediate = $qualification['institution_name'] . ' (' . $qualification['passing_year'] . ', ' . $qualification['percentage'] . '%)';
                             break;
                         case 'Graduation':
-                            $graduation = $qualification->institution_name . ' (' . $qualification->passing_year . ', ' . $qualification->percentage . '%)';
+                            $graduation = $qualification['institution_name'] . ' (' . $qualification['passing_year'] . ', ' . $qualification['percentage'] . '%)';
                             break;
                         case 'Post Graduation':
-                            $postGraduation = $qualification->institution_name . ' (' . $qualification->passing_year . ', ' . $qualification->percentage . '%)';
+                            $postGraduation = $qualification['institution_name'] . ' (' . $qualification['passing_year'] . ', ' . $qualification['percentage'] . '%)';
                             break;
                     }
                 }
